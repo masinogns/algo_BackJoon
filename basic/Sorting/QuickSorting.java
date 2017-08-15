df9d13b455d8d50b4e2f868f2a2c4ce540766ed9
백기훈 <masinogns@gmail.com>
2017-08-16 00:04:17 +0900

    Test Source를 보면 Refactoring의 필요성이 느껴진다. Test Source code가 new 해주는 부분을 제외하고 모두 똑같다. 어떻게 Refactoring을 하면 좋을까?

---
+++ basic/Sorting/QuickSorting.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/**
+ * Created by masinogns on 2017. 8. 16..
+ */
+public class QuickSorting {
+    private ArrayList arrayList;
+
+    public void setArrayList(ArrayList arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public ArrayList getArrayList() {
+        return arrayList;
+    }
+
+    public void sorting(ArrayList arrayList) {
+
+    }
+
+    public ArrayList sortingResult() {
+        return null;
+    }
+}
--- basic/SortingTest/SortingTest.java
+++ basic/SortingTest/SortingTest.java
@@ -8,10 +8,23 @@ import static org.junit.Assert.*;
 /**
  * Created by masinogns on 2017. 8. 15..
  */
-public class SelectionSortingTest {
+public class SortingTest {
     @Test
-    public void test(){
+    public void selctionSorting(){
         SelectionSorting application = new SelectionSorting();
+
+        ArrayList testArray = new ArrayList(Arrays.asList(80,75,10,60,15,49,12,25));
+        ArrayList answerArray = new ArrayList(Arrays.asList(10,12,15,25,49,60,75,80));
+
+        application.setArrayList(testArray);
+        application.sorting(application.getArrayList());
+
+        assertEquals(answerArray, application.sortingResult());
+    }
+
+    @Test
+    public void quickSorting(){
+        QuickSorting application = new QuickSorting();
 
         ArrayList testArray = new ArrayList(Arrays.asList(80,75,10,60,15,49,12,25));
         ArrayList answerArray = new ArrayList(Arrays.asList(10,12,15,25,49,60,75,80));
