5bf312e0145b3dc30299961ca85858d4e161c3a9
백기훈 <masinogns@gmail.com>
2017-08-16 12:17:44 +0900

    Bubble Sort algorithm ; 인접한 두 수를 비교해서 큰 수를 작은 수 뒤로 보내는 알고리즘 첫 번째 for문으로 이동 연산을 하고 두 번째 for문으로 비교 연산을 한다.

--- basic/Sorting/QuickSorting.java
+++ basic/Sorting/QuickSorting.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by masinogns on 2017. 8. 16..
@@ -14,11 +15,61 @@ public class QuickSorting {
         return arrayList;
     }
 
-    public void sorting(ArrayList arrayList) {
+    public void sorting(ArrayList S) {
+        if (S.size() < 2) return; // Nothing needs to be done if S has zero or one element)
+
+        // Divide: If S has at least two elements, select a specific element x from S, which is called the pivot.
+        ArrayList<Integer> L = new ArrayList<Integer>(); // L, storing the elements in S less than pivot
+        ArrayList<Integer> E = new ArrayList<Integer>(); // E, storing the elements in S equal to pivot
+        ArrayList<Integer> G = new ArrayList<Integer>(); // G, storing the elements in S greater than pivot
+
+        int pivot = (int)S.get(S.size() - 1);
+        E.add(pivot);
+        S.remove(S.size() - 1);
+
+        while (S.size() > 0) {
+            if ((int)S.get(0) < pivot) {
+                L.add((int)S.get(0));
+            } else if ((int)S.get(0) == pivot) {
+                E.add((int)S.get(0));
+            } else {
+                G.add((int)S.get(0));
+            }
+            S.remove(0);
+        }
+
+        // Conquer: Recursively sort sequences L and G.
+        sorting(L);
+        sorting(G);
+
+        showArray(L);
+        showArray(E);
+        showArray(G);
+
+        // Combine: Put back the element into S in order by first inserting the elements of L, then those of E, and finally those of G.
+        for (int i = 0; i < L.size(); i++) {
+            S.add((int)L.get(i));
+        }
+        for (int i = 0; i < E.size(); i++) {
+            S.add((int)E.get(i));
+        }
+        for (int i = 0; i < G.size(); i++) {
+            S.add((int)G.get(i));
+        }
 
     }
 
+    private void showArray(ArrayList<Integer> arrayList) {
+        Iterator iterator = arrayList.iterator();
+
+        while (iterator.hasNext()) System.out.print(iterator.next());
+        System.out.println("");
+    }
+
+
+//    출처: http://woongheelee.com/entry/자바로-구현한-퀵-소트quick-sort-자바-소스-코드 [노트정리]
+
     public ArrayList sortingResult() {
-        return null;
+        return arrayList;
     }
 }
--- basic/SortingTest/SortingTest.java
+++ basic/SortingTest/SortingTest.java
@@ -9,12 +9,18 @@ import static org.junit.Assert.*;
  * Created by masinogns on 2017. 8. 15..
  */
 public class SortingTest {
+
+    ArrayList testArray;
+    ArrayList answerArray;
+
+    public SortingTest() {
+        testArray = new ArrayList(Arrays.asList(80,75,10,60,15,49,12,25));
+        answerArray = new ArrayList(Arrays.asList(10,12,15,25,49,60,75,80));
+    }
+
     @Test
     public void selctionSorting(){
         SelectionSorting application = new SelectionSorting();
-
-        ArrayList testArray = new ArrayList(Arrays.asList(80,75,10,60,15,49,12,25));
-        ArrayList answerArray = new ArrayList(Arrays.asList(10,12,15,25,49,60,75,80));
 
         application.setArrayList(testArray);
         application.sorting(application.getArrayList());
@@ -26,8 +32,15 @@ public class SortingTest {
     public void quickSorting(){
         QuickSorting application = new QuickSorting();
 
-        ArrayList testArray = new ArrayList(Arrays.asList(80,75,10,60,15,49,12,25));
-        ArrayList answerArray = new ArrayList(Arrays.asList(10,12,15,25,49,60,75,80));
+        application.setArrayList(testArray);
+        application.sorting(application.getArrayList());
+
+        assertEquals(answerArray, application.sortingResult());
+    }
+
+    @Test
+    public void bubleSotring(){
+        BubbleSorting application = new BubbleSorting();
 
         application.setArrayList(testArray);
         application.sorting(application.getArrayList());
